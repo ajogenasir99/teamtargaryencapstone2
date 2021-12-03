a73104a9c1d6177f7154a7d9ab0d77a5e08bc538
Nas <nazylamar9@gmail.com>
2021-12-03 09:06:03 +0100

    final hopefully

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import shopHome from '../components/shopHome.vue'
+import CartPage from '../components/CartPage.vue'
+import homepage from '../components/homepage.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+     {
+       path: '/',
+       name: 'Home',
+       component: homepage
+     }, {
+       path: '/shophome',
+       name: 'shophome',
+       component: shopHome
+     }, {
+       path: '/cart',
+       name: 'cart',
+       component: CartPage
+     }
+  ]
+})
